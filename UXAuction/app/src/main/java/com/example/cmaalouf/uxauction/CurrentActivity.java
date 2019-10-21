e5587d6b3ebf9573a573f626dce4dcf4cbe39f2a
cmmaalouf <cmmaalouf@loyola.edu>
2019-10-21 16:33:38 -0400

    here is the working app UI

---
+++ UXAuction/app/src/main/java/com/example/cmaalouf/uxauction/CurrentActivity.java
@@ -0,0 +1,15 @@
+package com.example.cmaalouf.uxauction;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CurrentActivity extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_currentbids);
+
+    }
+}
---
+++ UXAuction/app/src/main/java/com/example/cmaalouf/uxauction/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.cmaalouf.uxauction;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+
+    protected void login(View view)
+    {
+        Intent myIntent = new Intent( this,
+                ScrollingActivity.class );
+        this.startActivity( myIntent );
+
+    }
+}
