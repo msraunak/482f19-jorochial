729b6b8c521bc259532120deb594239c58ff0fbc
jdbennett94 <jdbennett@loyole.edu>
2019-10-25 01:11:15 -0400

    ChangePasswords -> Settings.php, finished Dashboard.php

--- WebUX/DashboardPage.php
+++ WebUX/DashboardPage.php
@@ -8,6 +8,11 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script  type="text/javascript" src="formValidation.js"></script>
     <title>Dashboard</title>
+    <style>
+        .content{
+          min-height:70vh;
+        }
+    </style>
   </head>
   <body>
 
@@ -45,6 +50,87 @@
 
     <div class="content">
 
+      <div class="container">
+        <h3 class="text-center">Your Dashboard: </h3>
+
+        <!-- Large input -->
+        <div class="md-form form-lg">
+          <input type="text" id="inputLGEx" class="form-control form-control-lg" placeholder="search for an existing item at auction">
+          <label for="inputLGEx"></label>
+        </div>
+      </div>
+
+
+      <h5 class="text-center">See what is currently on bid:</h5>
+
+
+      <div class="card mb-3" >
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://i.etsystatic.com/10797882/r/il/00ee9c/1373183800/il_794xN.1373183800_3udm.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Chessboard</h5>
+              <p class="card-text">This chessboard was owned by King George back in 1945, seeing use by over three generation sof royal family....</p>
+              <p class="card-text"><small class>Current Bid: $1,000.00</small></p>
+              <p class="card-text"><small class="text-muted">Donated by: The Royal Family</small></p>
+              <a href="#" class="btn stretched-link"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3" >
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://upload.wikimedia.org/wikipedia/en/c/c0/Murder_on_the_Orient_Express_First_Edition_Cover_1934.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">Agatha Christie Novel</h5>
+              <p class="card-text">Written by accalimed novelist Agatha Christie this is a copy of her famous Murder On The Orient Express with annotations
+              from her written in the margins and a signature at the beginning of the novel. </p>
+              <p class="card-text"><small class=>Current Bid: $14,000.00</small></p>
+              <p class="card-text"><small class="text-muted">Donated by: The Agatha Foundation</small></p>
+              <a href="#" class="btn stretched-link"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="card mb-3">
+        <div class="row no-gutters">
+          <div class="col-md-4">
+            <img src="https://i.imgur.com/DsOqW3G.jpg" class="card-img" alt="...">
+          </div>
+          <div class="col-md-8">
+            <div class="card-body">
+              <h5 class="card-title">1988 Shelby Mustang</h5>
+              <p class="card-text">If you love classic cars, then look no further than this fully refurbished and detailed 1968 Shelby Mustang.Sporting all the original features,
+              this is the true classic driving experience. </p>
+              <p class="card-text"><small class>Current Bid: $140,000.00</small></p>
+              <p class="card-text"><small class="text-muted">Donated by: Auto Retailers of America</small></p>
+              <a href="#" class="btn stretched-link"></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
+      <nav aria-label="Page navigation example">
+        <ul class="pagination justify-content-center">
+          <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+          <li class="page-item"><a class="page-link" href="#">1</a></li>
+          <li class="page-item"><a class="page-link" href="#">2</a></li>
+          <li class="page-item"><a class="page-link" href="#">3</a></li>
+          <li class="page-item"><a class="page-link" href="#">Next</a></li>
+        </ul>
+      </nav>
+
+
+
       <!--
       $servername = "cs-database.cs.loyola.edu";
       $username = "jdbennett";
@@ -69,11 +155,9 @@
 
       $mysqli->close();
       -->
-    </div>
 
 
 
-<!--
     <div class="footer">
       <h3> Contact Us </h3>
       <p>Main Campus<br>
@@ -91,7 +175,6 @@
         Columbia, MD 21045<br>
         410-617-7600</p>
     </div>
--->
 
   </body>
 </html>
