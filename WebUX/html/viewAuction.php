d88f5ec5424f953700e76597fd6db0c370fa15b4
jdbennett94 <jdbennett@loyole.edu>
2019-11-19 13:21:35 -0500

    Settings changed to Bootstrap version, adding new admin func next

--- WebUX/html/viewAuction.php
+++ WebUX/html/viewAuction.php
@@ -4,7 +4,7 @@ $auction_description = "This auction is in support of the XYZ group and features
 $auction_start_date = date("l jS \of F Y h:i:s A", 1530054626);;
 $auction_end_date= date("l jS \of F Y h:i:s A", 1530154626);
 $auction_charity = "The Children's Project";
-$item_arr = array("Chessboard","This chessboard was owned by King George back in 1945, seeing use by over three generations of royal family. It was sold to the French after the Battle of 1765 and was gifted to the King after the former owner when in against a Sicilian when death was on the line.","The Royal Family",1000, 500.00,100.02,)
+$item_arr = array("Chessboard","This chessboard was owned by King George back in 1945, seeing use by over three generations of royal family. It was sold to the French after the Battle of 1765 and was gifted to the King after the former owner when in against a Sicilian when death was on the line.","The Royal Family",1000, 500.00,100.02,);
 
 include 'viewAuction.html';
 ?>
