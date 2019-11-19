5a357d0f11c1237b13747224263b38a8a07dec6a
alyssaschilke <arschilke@loyola.edu>
2019-11-19 17:20:16 -0500

    alert color

--- WebUX/html/Settings.php
+++ WebUX/html/Settings.php
@@ -2,7 +2,7 @@
 
 if (isset($_SESSION["adminAdd"])){
   if($_SESSION["adminAdd"] == True){
-    $alert =  '<div class="alert alert-success alert-dismissible fade show" role="alert">
+    $alert =  '<div class="alert alert-secondary alert-dismissible fade show" role="alert">
        New Admin added.
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
@@ -10,7 +10,7 @@ if (isset($_SESSION["adminAdd"])){
     </div>';
   }
   else{#($_SESSION["adminAdd"] == False){
-    $alert =  '<div class="alert alert-warning alert-dismissible fade show" role="alert">
+    $alert =  '<div class="alert alert-danger alert-dismissible fade show" role="alert">
        New Admin not added.
       <button type="button" class="close" data-dismiss="alert" aria-label="Close">
       <span aria-hidden="true">&times;</span>
