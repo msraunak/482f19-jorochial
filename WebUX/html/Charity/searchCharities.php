cfbba03122ee226aeccfba43f596fc39a70e33c4
arschilke <arschilke@loyola.edu>
2019-12-15 01:38:35 -0500

    CHARITY FIX

--- WebUX/html/Charity/searchCharities.php
+++ WebUX/html/Charity/searchCharities.php
@@ -32,6 +32,11 @@ if (isset($_SESSION["charityNotice"])) {
     </div>';
     }
 }
+if(isset($_GET["query"])){
+  $query = htmlspecialchars($_GET["query"]);
+}else{
+  $query = htmlspecialchars("Car");
+}
 if(!isset($_GET["page"])){
   $pageNumber = 1;
 }
@@ -83,7 +88,7 @@ function charityTable($pageNum, $tableSize ,$mysqli, $query){
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <script type="text/javascript" src="../../js/formValidation.js"></script>
-    <title>Dashboard</title>
+      <title>Search Results</title>
     <style>
       .content {
         min-height: 70vh;
