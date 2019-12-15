d318c5895cc8c1df353e6e1be453194ff7855c69
alyssaschilke <arschilke@loyola.edu>
2019-12-14 21:25:30 -0500

    footer

--- WebUX/html/auctionserver.php
+++ WebUX/html/auctionserver.php
@@ -9,10 +9,27 @@ if ($mysqli->connect_error) {
 // Select all of our stocks from table 'stock_tracker'
 $sql = "SELECT * FROM Item";
 //there is no stock_tracker table what are
-
-
 $result = $mysqli->query($sql);
 
-$resultArray =  json_encode(($result->fetch_assoc()));
-echo $resultArray;
+$resultArray = '{ "items": [';
+$tempArray= array();
+
+//test
+//echo json_encode($result->fetch_assoc());
+//echo json_encode($result->fetch_assoc());
+//$row =0;
+while($row=$result->fetch_assoc())
+{
+$tempArray = $row;
+//echo json_encode($tempArray);
+//echo json_encode($result->fetch_assoc());
+//array_push($resultArray, $tempArray)
+$resultArray.=json_encode($tempArray).',';// $tempArray);
+echo $resultsArray;
+}
+
+$resultArray = substr($resultArray, 0, -1);
+$resultArray.=']}';
+ echo $resultArray;
+//echo $resultArray;
 ?>
