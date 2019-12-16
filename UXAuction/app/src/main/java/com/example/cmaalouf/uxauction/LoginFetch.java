fabf08e5be676fc438eb289277b35eccd97adad1
cmmaalouf <cmmaalouf@loyola.edu>
2019-12-16 05:02:34 -0500

    started to work on login in validation, ui fixed annoying edit text keyboard

--- UXAuction/app/src/main/java/com/example/cmaalouf/uxauction/LoginFetch.java
+++ UXAuction/app/src/main/java/com/example/cmaalouf/uxauction/LoginFetch.java
@@ -9,26 +9,26 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
 
-import static com.example.cmaalouf.uxauction.ScrollingActivity.items;
-import static com.example.cmaalouf.uxauction.ScrollingActivity.query;
+import static com.example.cmaalouf.uxauction.MainActivity.pass;
+import static com.example.cmaalouf.uxauction.MainActivity.username;
+import static com.example.cmaalouf.uxauction.MainActivity.enter;
 import static java.lang.String.valueOf;
 
-public class SearchFetchData extends AsyncTask<String,String,String> {
+public class LoginFetch extends AsyncTask<String,String,String> {
 
-    String data;
-    String data_parse;
-    String single_parse;
     private Context ctx;
-    public SearchFetchData(Context context)
+    public LoginFetch(Context context)
     {
         this.ctx=context;
 
@@ -39,23 +39,36 @@ public class SearchFetchData extends AsyncTask<String,String,String> {
     protected void onPreExecute()
     {
         super.onPreExecute();
-        data ="";
     }
 
 
+
     @Override
-    protected String doInBackground(String... voids) {
+    protected String doInBackground(String... strings) {
         try {
             //items.add("before url");
-            Log.w("QUERY", ""+query);
-            URL url = new URL("http://jorochial.cs.loyola.edu/php/search.php?query="+query);
+             String user = strings[0];
+            String passw = strings[1];
+            URL url = new URL("http://jorochial.cs.loyola.edu/php/index.php");
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             //Log.w("HHTP STAT", valueOf(con.getResponseCode()));
-            con.setRequestMethod("GET");
-
+            con.setRequestMethod("POST");
+            con.setRequestProperty("username","hfranceschi");
+            //con.setRequestProperty("password","pwd");
             con.setDoOutput( true );
             con.setDoInput(true);
-        ;
+
+            OutputStream os = con.getOutputStream();
+            Log.w("USR", ""+user);
+            Log.w("PSS","hiii"+passw);
+            Log.w("OS ",os.toString());
+            BufferedWriter writer = new BufferedWriter( new OutputStreamWriter(os, "iso-8859-1"));
+            //writer.write(username);
+            //writer.write(pass);
+            writer.flush();
+            writer.close();
+            os.close();
+
             InputStream inputStream = con.getInputStream();
             Log.w("fetchdata ",inputStream.toString());
             //items.add("after input stream");
@@ -63,27 +76,17 @@ public class SearchFetchData extends AsyncTask<String,String,String> {
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
             String line = "";
             String result = "";
-            data = "";
+
             //items.add("past buffer");
             while ((line=bufferedReader.readLine()) != null) {
                 result += line;
                 //json+=line;
                 Log.w("echo from php", result);
-                data = data +line;
+
 
             }
-            data = result;
-            JSONArray jsonArray = new JSONArray(result);
-            for(int i = 0; i<jsonArray.length(); i++) {
-                JSONObject jsonObject = (JSONObject) jsonArray.get(i);
-                String name = jsonObject.get("itemName").toString();
-                String desc =jsonObject.get("description").toString();
-                String donor = jsonObject.get("donorName").toString();
-                Double startingBid = Double.valueOf(jsonObject.get("startingBid").toString());
-                Double minInc = Double.valueOf(jsonObject.get("minimumBidInc").toString());
-                Item item = new Item(name, desc, startingBid,minInc,donor);
-                items.add(item);
-            }
+
+
 
             bufferedReader.close();
             inputStream.close();
@@ -93,11 +96,9 @@ public class SearchFetchData extends AsyncTask<String,String,String> {
         } catch (IOException e) {
             e.printStackTrace();
             // items.add("IO");
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
 
-        return data;
+        return "";
     }
     @Override
     protected  void onPostExecute(String aVoid)
@@ -107,15 +108,4 @@ public class SearchFetchData extends AsyncTask<String,String,String> {
 
     }
 
-    public String getData(){
-
-        return data;
-    }
-
-
 }
-
-
-
-
-
