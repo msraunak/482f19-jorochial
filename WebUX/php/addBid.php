d562681bed8b2e1b77384baa3f4400622b1cfd8d
arschilke <arschilke@loyola.edu>
2019-12-16 14:40:48 -0500

    add Bid php

---
+++ WebUX/php/addBid.php
@@ -0,0 +1,41 @@
+<?php
+
+require_once 'config.php';
+
+$htmlOutput = "";
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " .  $mysqli->connect_error);
+}
+
+#echo "Connected successfully";
+$bid = array($_GET["username"],$_GET["itemId"],$_GET["amount"]);
+
+$sql = "SELECT ItemName FROM Item WHERE id = ". $_GET["itemId"];
+
+$result = $mysqli->query($sql);
+$row = $result->fetch_assoc();
+
+$itemName = $row["ItemName"];
+
+  $sql .= 'INSERT INTO admin(bidderUName, amount, itemId, ItemName) VALUES';
+
+  $bid[0] = htmlspecialchars(trim($bid[0]));
+  $bid[1] = htmlspecialchars(trim($bid[1]));
+  $bid[2] = htmlspecialchars(trim($bid[2]));
+  $bid[3] = $itemName;
+  $sql .= '("'.$bid[0].'",'. $bid[1] .','. $bid[2] .',"'. $bid[3] .'")';
+  $sql .= ";";
+  #echo $sql;
+  if ($mysqli->query($sql) === TRUE) {
+      $htmlOutput .= "Success";
+  }
+  else{
+    $htmlOutput .= "Failed ". $mysqli->error;
+
+  }
+  echo $htmlOutput;
+
+?>
