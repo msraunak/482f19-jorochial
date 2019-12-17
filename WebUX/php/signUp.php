cd7a50d4617f403b108f48bd4d2c705025da6249
arschilke <arschilke@loyola.edu>
2019-12-17 01:29:16 -0500

    sign up

--- WebUX/php/signUp.php
+++ WebUX/php/signUp.php
@@ -8,15 +8,15 @@ $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 if ($mysqli->connect_error) {
     die("Connection failed: " .  $mysqli->connect_error);
 }
-//$bidder = array($_Post["username"],$_Post["email"],$_Post["password"],$_Post["fname"],$_Post["lname"]);
-$bidder = array($_Post["fname"],$_Post["lname"],$_Post["email"],$_Post["username"],$_Post["pwd"]);
+//$bidder = array($_POST["username"],$_POST["email"],$_POST["password"],$_POST["fname"],$_POST["lname"]);
+$bidder = array($_POST["fname"],$_POST["lname"],$_POST["email"],$_POST["username"],$_POST["pwd"]);
 
 $sql = 'INSERT INTO Bidder (fname, lname, email, username, pwd) VALUES';
 
-$bidder[0] = htmlspecialchars(trim($bid[0]));
-$bidder[1] = htmlspecialchars(trim($bid[1]));
-$bidder[2] = htmlspecialchars(trim($bid[2]));
-$bidder[3] = htmlspecialchars(trim($bid[3]));
+$bidder[0] = htmlspecialchars(trim($bidder[0]));
+$bidder[1] = htmlspecialchars(trim($bidder[1]));
+$bidder[2] = htmlspecialchars(trim($bidder[2]));
+$bidder[3] = htmlspecialchars(trim($bidder[3]));
 $bidder[4]= password_hash(htmlspecialchars(trim($bidder[4])), PASSWORD_DEFAULT);
 $sql .= '("'.$bidder[0].'","'. $bidder[1] .'","'. $bidder[2] .'","'. $bidder[3] .'","'.$bidder[4].'")';
   $sql .= ";";
