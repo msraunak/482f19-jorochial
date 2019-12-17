e707d7412ede70495c7a4646231d70d47137e6f7
arschilke <arschilke@loyola.edu>
2019-12-16 22:50:02 -0500

    auto_bid

---
+++ WebUX/php/auto_bidAsk.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once 'config.php';
+
+$htmlOutput = "";
+
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+// Check connection
+if ($mysqli->connect_error) {
+    die("Connection failed: " .  $mysqli->connect_error);
+}
+
+#echo "Connected successfully";
+$bid = $_GET["bidId"];
+
+$sql = "SELECT currentBid, minimumBidInc FROM Bids WHERE bidId = ". $_GET["bidId"];
+
+$result = $mysqli->query($sql);
+
+$resultArray = '[';
+$tempArray= array();
+
+while($row=$result->fetch_assoc())
+{
+$tempArray = $row;
+$resultArray.=json_encode($tempArray).',';// $tempArray);
+echo $resultsArray;
+}
+
+$resultArray = substr($resultArray, 0, -1);
+$resultArray.=']';
+ echo $resultArray;
+
+?>
