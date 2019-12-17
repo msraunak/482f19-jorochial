16a5d8c423474437252fc12e365e409256b8d545
jdbennett94 <jdbennett@loyole.edu>
2019-12-16 22:44:36 -0500

    resetPassword.php done, added to Settings.php

--- WebUX/html/Admin/resetPwd.php
+++ WebUX/html/Admin/resetPwd.php
@@ -14,21 +14,23 @@ $userCurrentPassword = $_POST['currentPwd'];
 $newPassword1 = $_POST['newPwd1'];
 $newPassword2 = $_POST['newPwd2'];
 $mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
 if ($mysqli->connect_error) {
     die("Connection failed: " .  $mysqli->connect_error);
 }
 
-//Selects passwor dof that user
-$sqlValidateCredentials = "SELECT * from admin where uname = $username";
+//Selects password of that user
+$sqlValidateCredentials = "SELECT * from admin where uname = '$username'";
 $result = $mysqli->query($sqlValidateCredentials);
 
 //Does that user exist
 if ($result->num_rows > 0) {
-    // output data of each row
+    // Gets password in database taht matches that username
     while($row = $result->fetch_assoc()) {
         $trueCurrentPassword = $row['pwd'];
     }
 
+    //Determines if passwords match of user
     if ($newPassword1 !== $newPassword2) {
       goto a;
     }
@@ -45,24 +47,22 @@ if ($result->num_rows > 0) {
           echo '<script language="javascript">';
           echo 'alert("Password successfully changed")';
           echo '</script>';
-          header("Refresh:5; url=Settings.php");
-          exit;
+          header("Refresh:1; url=http://jorochial.cs.loyola.edu/html/Settings.php");
         }
 
       }
       else {
         echo '<script language="javascript">';
-        echo 'alert("Sorry your password does not match")';
+        echo 'alert("Sorry your password does not match database")';
         echo '</script>';
-        header("Refresh:5; url=Settings.php");
-        exit;
+        header("Refresh:1; url=http://jorochial.cs.loyola.edu/html/Settings.php");
       }
 
     }
 }
 else{
   echo '<script language="javascript">';
-  echo 'alert("hmmmm")';
+  echo 'alert("That username does not exist! Please try again")';
   echo '</script>';
 }
 
@@ -72,96 +72,15 @@ a:
   echo '<script language="javascript">';
   echo 'alert("Sorry those passwords do not match")';
   echo '</script>';
-  header("Refresh:5 ; url=Settings.php");
-  exit;
+  header("Refresh:1 ; url=http://jorochial.cs.loyola.edu/html/Settings.php");
 
 b:
   echo '<script language="javascript">';
   echo 'alert("Please enter the correct credentials")';
   echo '</script>';
-  header("Refresh:5; url=Settings.php");
-  exit;
+  header("Refresh:1; url=http://jorochial.cs.loyola.edu/html/Settings.php");
 
-
-
-
-
-  end:
-  echo '<script language="javascript">';
-  echo 'window.location.reload()';
-  echo '</script>';
-  header("Refresh:5; url=Settings.php");
-  exit;
-
-/*
-$_SESSION["resetPassword"] = False;
-$htmlOutput = "";
-
-$unameFromSettings = $_POST['resetUsername'];
-
-
-// Check connection
-$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if ($mysqli->connect_error) {
-    die("Connection failed: " .  $mysqli->connect_error);
-}
-
-//Determines if uname exists
-$sqlTest = 'SELECT pwd from admin where uname = "'.$_POST['resetUsername'].'"';
-$resultUname = $mysqli->query($sqlTest);
-if ($resultUname->num_rows = 1) {
-    $htmlOutput .= "That's the right username";
-}
-else{
-    $htmlOutput .= "That was the wrong username";
-    $_SESSION["resetPassword"] = $htmlOutput;
-    //header("Location: http://jorochial.cs.loyola.edu/html/Settings.php");
-    //exit;
-}
-
-//Populates row with user profile; username and password
-$row = $resultUname->fetch_assoc( );
-$hashedPassword = $row['password'];
-
-//If passwords match
-if ($_POST["newPwd1"] == $_POST["newPwd2"]){
-
-      //Verifies username password
-      if(password_verify($_POST['currentPwd'], $hashedPassword )){
-
-        $newHashedPassword = password_hash($_POST['newPwd1'], PASSWORD_DEFAULT);
-        $sqlUpdate = "UPDATE admin set pwd = '$newHashedPassword' where uname = '$unameFromSettings'";
-        //$resultPassword = $mysqli->query($sqlUpdate);
-
-        #echo $sql;
-        if ($mysqli->query($sqlUpdate) === TRUE) {
-            $htmlOutput .= "Users added successfully";
-        }
-        else{
-          $htmlOutput .= "Insertion Failed: ". $mysqli->error;
-        }
-
-        #echo $htmlOutput;
-        $_SESSION["resetPassword"] = True;
-
-      }
-      else{
-        $htmlOUtput .= "Passwords do not match";
-        $_SESSION["resetPassword"] = False;
-      }
-
-    }
-else {
-  #echo "vaildation fail";
-  $_SESSION["resetPassword"] = False;
-}
-
-
-$htmlOUtput = "Password is " .$newHashedPassword;
-$_SESSION["passwordMessage"] = $htmlOutput;
-header("Location: http://jorochial.cs.loyola.edu/html/Settings.php");
-exit;
-*/
-
+end:
+  header("Refresh:0; url=http://jorochial.cs.loyola.edu/html/Settings.php");
 
 ?>
