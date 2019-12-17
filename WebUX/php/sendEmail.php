21b6eec069532a3f01c5bf27581b3b940a7d6014
arschilke <arschilke@loyola.edu>
2019-12-17 05:12:06 -0500

    update

--- WebUX/php/sendEmail.php
+++ WebUX/php/sendEmail.php
@@ -14,10 +14,9 @@ if ($mysqli->connect_error) {
  $to_email = $_GET['emailAddress'];
 
 
-$sql = "SELECT * FROM Bidder WHERE email = '$to_email'";
+$sql = "SELECT * FROM Bidder WHERE email = '$to_email';";
 $result = $mysqli->query($sql);
 if ($result->num_rows > 0) {
-  echo "inside";
   $subject = $_GET['emailSubject'];
   $message = $_GET['emailMessage'];
   $headers = 'From: noreply@jorochial.cs.loyola.edu';
