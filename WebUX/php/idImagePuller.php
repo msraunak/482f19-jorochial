3e1037c72c9c8362cd555ddee0e57157e656ae5a
jdbennett94 <jdbennett@loyole.edu>
2019-12-16 23:38:37 -0500

    ImageData puller via id php, idImagePuller.php

---
+++ WebUX/php/idImagePuller.php
@@ -0,0 +1,29 @@
+<?php
+
+//Database components
+const DB_HOST = 'cs-database.cs.loyola.edu';
+const DB_USER = 'arschilke';
+const DB_PASS = '1737341';
+const DB_NAME = 'jorochial';
+$mysqli = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+if ($mysqli->connect_error) {
+    die("Connection failed: " .  $mysqli->connect_error);
+}
+
+//User input for id
+$userIdRequest = $_POST['id'];
+
+//Queries database
+$sqlIdStatement = "select imageData from Item where id = '$userIdRequest' LIMIT 1";
+$result = $mysqli->query($sqlIdStatement);
+
+while ($row = $result->fetch_assoc()) {
+
+  $imageDataString = $row['imageData'];
+
+}
+
+return $imageDataString;
+
+$mysqli->close(); 
+ ?>
